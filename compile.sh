57081a5b8c001a6eba9a06436ab15d230cb72658
José María Valera Reales <chemaclass@outlook.es>
2014-07-10 21:58:23 +0200

    Mejoro compile.sh. Encuentro todos los ficheros .c y los muestro

--- compile.sh
+++ compile.sh
@@ -6,29 +6,35 @@
 # by @Chemaclass           #
 ############################
 
-baseFile="./src/base.c"
+allSrcFiles=$(ls ./src/*.c)
+# Nombres de los ficheros .c
+allSrcFilesNames=$(ls -l ./src | awk '{
+    split($0,line," ")
+    filename = line[9]
+    ext = substr(filename,length(filename)-1)
+    if (ext == ".c") printf("%s\n", filename)    
+}')
+echo "All src files are:" $allSrcFilesNames
+baseFile="base"
+mainFile="main"
 std="c99"
 
-function __main__ {    
-    if [ $1 ]; then
-        # if not exists directory, we must create
-        if [ ! -d bin ]; then
-            mkdir ./bin
-        fi 
-        # compile
-        gcc -Wall -std=$std $baseFile -o ./bin/$1 ./src/$1.c 
-        # execute if exists
-        if [ -f ./bin/$1 ]; then
-            ./bin/$1
-        else
-            echo "Doesn't exist the executable $1"
-        fi
+function main {
+    # if not exists directory, we must create
+    if [ ! -d bin ]; then
+        mkdir ./bin
+    fi 
+    # compile
+    gcc -Wall -std=$std $allSrcFiles -o ./bin/$mainFile
+    # execute if exists
+    if [ -f ./bin/$mainFile ]; then
+        ./bin/$mainFile
     else
-        echo "?>> Help: $0 <programName>"
+        echo "Doesn't exist the executable ./bin/$mainFile"
     fi
 }
 
 echo "================BEGIN==================="
-__main__ $1
+main
 echo "=================END===================="
 
