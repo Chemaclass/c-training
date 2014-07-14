cae4d7c662451571b167b3ade5757cfa1cb33887
José María Valera Reales <chemaclass@outlook.es>
2014-07-14 23:36:27 +0200

    Añado linkedlist básica en lists.c

--- src/headers/lists.h
+++ src/headers/lists.h
@@ -1,4 +1,19 @@
 #pragma once
 
+typedef struct node {
+  char *value;
+  struct node *next;
+} Node;
+
+typedef struct list {
+	Node *start;
+	Node *end;
+	int size;
+} List;
+
 void listsMain();
+void ListInit(List *list);
+int ListPrepend(List *list, char *value);
+int ListAppend(List *list, char *value);
+void ListToString(List *list);
 
--- src/lists.c
+++ src/lists.c
@@ -1,14 +1,77 @@
 #include "headers/base.h"
 #include "headers/lists.h"
 
-typedef struct node {
-  int value;
-  struct node *next;
-} Node;
-
-
 void listsMain(){
 	printChapter("Lists");
 	
-	
+	List *list = (List*) malloc(sizeof(List));
+	ListInit(list);
+	char str[50];
+	for(int i = 0; i < 10; i++) {
+		sprintf(str, "%d", i);
+		ListPrepend(list, str);
+	}
+	for(int i = 1; i < 10; i++) {
+		sprintf(str, "%d", i);
+		ListAppend(list, str);
+	}
+	ListToString(list);
 }
+
+// Init
+void ListInit(List *list) {
+	list->start = NULL;
+	list->end = NULL;
+	list->size = 0;
+}
+
+// Prepend
+int ListPrepend(List *list, char *value) {
+	Node *newNode;
+	if((newNode = (Node*)malloc(sizeof(Node))) == NULL) {
+		return -1;
+	}
+	if((newNode->value = (char*) malloc(50*sizeof(char))) == NULL) {
+		return -1;
+	}
+	
+	strcpy(newNode->value, value);
+
+	newNode->next = list->start;
+	list->start = newNode;
+	list->size++;
+	return 0;
+}
+
+// Append
+int ListAppend(List *list, char *value) {
+	Node *newNode, *aux = list->start;
+	if ((newNode = (Node*) malloc(sizeof(Node))) == NULL) {
+		return -1;
+	}
+	if((newNode->value = (char*)malloc(50*sizeof(char))) == NULL) {
+		return -1;
+	}
+
+	strcpy(newNode->value, value);
+	
+	while (aux->next != NULL) {
+		aux = aux->next;
+	}
+	
+	aux->next = newNode;
+	
+	return 0;
+}
+
+// toString
+void ListToString(List *list) {
+	Node *aux = list->start;
+	printf("List{");
+	while( aux != NULL) {
+		printf("%s,", aux->value);
+		aux = aux->next;
+	}
+	printf("}\n");
+}
+
